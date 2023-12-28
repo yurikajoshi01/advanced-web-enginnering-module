26613017f86850f304f2e96a622e02493e3cf8f4
c7297928 <y.joshi8926@student.leedsbeckett.ac.uk>
2023-12-28 23:24:27 +0000

    updated css color

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,15 +1,15 @@
 <nav class = "flex justify-start">
-    <div class="m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300" >
+    <div class="m-5 shadow-2xl rounded-sm bg-purple-400 border-purple-200" >
         <a href="{{ route('home') }}" class="text-gray-700 p-5 font-semibold text-3xl font-mono">
                 Home
         </a>
     </div> 
-    <div class="m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300" >
+    <div class="m-5 shadow-2xl rounded-sm bg-purple-400 border-purple-200" >
         <a href="{{ route('index') }}" class="text-gray-700 p-5 font-semibold text-3xl font-mono">
                 Products
         </a>
     </div>    
-    <div class="m-5 shadow-2xl rounded-sm bg-yellow-300 border-blue-300" >
+    <div class="m-5 shadow-2xl rounded-sm bg-purple-400 border-purple-200" >
         <a href="{{ route('create') }}" class="text-gray-700 p-5 font-semibold text-3xl font-mono">
                 Add Products
         </a>
--- resources/views/product-template.blade.php
+++ resources/views/product-template.blade.php
@@ -1,15 +1,15 @@
 @if(Route::currentRouteName()=='index')
-<div class="p-2 bg-blue-100">
+<div class="p-2 bg-purple-200">
 @else 
-<div class="p-8 max-w-xl bg-blue-100">
+<div class="p-8 max-w-xl bg-purple-200">
 @endif
     <div class ="bg-white p-1 rounded-lg shadow-lg">
-        <h3 class="text-blue-700 mb-4 text-lg font-bold">{{$product['artist']}}</h3>
+        <h3 class="text-purple-600 mb-4 text-lg font-bold">{{$product['artist']}}</h3>
         <h3 class ="font-bold mb-2 text-gray-800">{{$product['title']}}</h3>
         <div class="flex justify-between">
             <p class="text-gray-700">£{{$product['price']/100}}</p>
             @if(Route::currentRouteName()=='index')
-                <button value="{{$product['id']}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full select-product">Select</button>
+                <button value="{{$product['id']}}" class="bg-purple-400 hover:bg-purple-600 text-white font-bold py-2 px-4 rounded-full select-product">Select</button>
             @endif
         </div>
     </div>
