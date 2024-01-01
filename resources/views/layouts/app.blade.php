5842c3d4fa265a6fc429379bd429d9272dcb539a
c7297928 <y.joshi8926@student.leedsbeckett.ac.uk>
2024-01-01 21:52:15 +0000

    added settings_dropdown.blade.php and modified app layout and app service provider, now shows users name with dropdown when logged in.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,10 +25,15 @@
             <h2 class = "font-bold text-lg self-center">AWE COMPONENT 2</h2>
         </div>  
         <div>
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
-            @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
-            @endif     
+        @auth
+        @include('layouts.settings_dropdown') 
+        @endauth
+        @guest
+         <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+          @if (Route::has('register'))
+               <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+          @endif  
+        @endguest   
         </div>
     </header>
             <main class="border-bottom-double border-2">
